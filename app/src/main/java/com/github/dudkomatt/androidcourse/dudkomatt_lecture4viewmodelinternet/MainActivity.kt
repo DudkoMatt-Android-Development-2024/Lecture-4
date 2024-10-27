88a21c894fdde7967eb8c0a337ea949388d7891f
DudkoMatt <26856709+DudkoMatt@users.noreply.github.com>
2024-10-27 15:49:48 +0300

    init: Hello world base

---
+++ app/src/main/java/com/github/dudkomatt/androidcourse/dudkomatt_lecture4viewmodelinternet/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.github.dudkomatt.androidcourse.dudkomatt_lecture4viewmodelinternet
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.github.dudkomatt.androidcourse.dudkomatt_lecture4viewmodelinternet.ui.theme.DudkoMattLecture4ViewModelInternetTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DudkoMattLecture4ViewModelInternetTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DudkoMattLecture4ViewModelInternetTheme {
+        Greeting("Android")
+    }
+}
